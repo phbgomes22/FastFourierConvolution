48eb04a6524c86b4da9c348346cbaf20def9b051
Pedro Gomes <phbgomes@hotmail.com>
2022-12-02 20:01:12 -0300

    remove print from train file

---
+++ test.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from config import Config
+from util import *
+from models import *
+
+
+
+
+def test():
+    pass 
--- train.py
+++ train.py
@@ -17,16 +17,15 @@ real_label = 1
 fake_label = 0
 
 
+
 # custom weights initialization called on netG and netD
 def weights_init(m):
     classname = m.__class__.__name__
-    print(classname)
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
-
 
 
 def get_generator():
