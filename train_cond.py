41797a6c962b865de43e8b6183bd63f0bf0c01ac
Pedro Gomes <phbgomes@hotmail.com>
2022-12-15 16:51:20 -0300

    fix none type expection in cbn

--- train_cond.py
+++ train_cond.py
@@ -25,10 +25,9 @@ def weights_init(m):
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        print(m)
-        print(classname)
-        nn.init.normal_(m.weight.data, 1.0, 0.02)
-        nn.init.constant_(m.bias.data, 0)
+        if m.weight.data is not None:
+            nn.init.normal_(m.weight.data, 1.0, 0.02)
+            nn.init.constant_(m.bias.data, 0)
 
 
 def get_generator():
