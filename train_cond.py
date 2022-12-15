8f37752d9b5b4248c714d45739db220db6030699
Pedro Gomes <phbgomes@hotmail.com>
2022-12-15 17:01:31 -0300

    add type to comparrison

--- train_cond.py
+++ train_cond.py
@@ -25,7 +25,7 @@ def weights_init(m):
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
-        if m is ConditionalBatchNorm2d:
+        if type(m) is ConditionalBatchNorm2d:
             nn.init.normal_(m.bn.weight.data, 1.0, 0.02)
             nn.init.constant_(m.bias.data, 0)
         else:
