d7e63a727f368bbe553d1c05da90433bf64e9110
Pedro Gomes <phbgomes@hotmail.com>
2023-01-12 17:04:45 -0300

    debug bn

--- train_cond.py
+++ train_cond.py
@@ -25,6 +25,7 @@ def weights_init(m):
     if classname.find('Conv') != -1:
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
+        print(classname)
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
 
