cc852d0374816e5ce99927a330a1b88180036185
Pedro Gomes <phbgomes@hotmail.com>
2023-01-13 17:40:24 -0300

    remove nonclass from non conditional

--- train_cond.py
+++ train_cond.py
@@ -55,8 +55,7 @@ def get_generator():
                                 num_classes= num_classes, image_size=image_size, 
                                 embed_size=embed_size).to(device) 
     else:
-        netG = Generator(nz=nz, nc=nc, ngf=ngf, 
-                                num_classes= num_classes, image_size=image_size, 
+        netG = Generator(nz=nz, nc=nc, ngf=ngf, image_size=image_size, 
                                 embed_size=embed_size).to(device)
         
 
@@ -87,7 +86,7 @@ def get_discriminator():
     DEBUG = config.DEBUG
 
     # Create the Discriminator
-    netD = Discriminator(nc=nc, ndf=ndf, num_classes=num_classes, image_size=image_size).to(device)
+    netD = Discriminator(nc=nc, ndf=ndf, image_size=image_size).to(device)
 
     # Handle multi-gpu if desired
     if (device.type == 'cuda') and (ngpu > 1):
