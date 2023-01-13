1342635379ce801442b77f42dabf23fe3b13262a
Pedro Gomes <phbgomes@hotmail.com>
2023-01-13 18:04:41 -0300

    trying to fix shape

--- models/conditional/cond_generator.py
+++ models/conditional/cond_generator.py
@@ -56,7 +56,7 @@ class CondGenerator(nn.Module):
         # latent vector z: N x noise_dim x 1 x 1 
         #embedding = self.ylabel(labels).unsqueeze(2).unsqueeze(3)
  
-        embedding = self.lbl_embed(labels)
+        embedding = self.lbl_embed(labels).unsqueeze(2).unsqueeze(3)
         z = input #self.yz(input)
         x = torch.cat([z, embedding], dim=1)
         x = x.view(input.shape[0], self.nz + self.embed_size, 1, 1) # pq nz * 2 ? pq não nz?
