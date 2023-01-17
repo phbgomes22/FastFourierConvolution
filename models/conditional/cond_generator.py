ec4d02b23f26095efb390803866cfce340a0c4fc
Pedro Gomes <phbgomes@hotmail.com>
2023-01-17 15:57:27 -0300

    fixing parameters of conv

--- models/conditional/cond_generator.py
+++ models/conditional/cond_generator.py
@@ -76,13 +76,13 @@ class CondCvGenerator(nn.Module):
         self.label_embed = nn.Embedding(num_classes, num_classes)
 
         self.label_conv = nn.Sequential(
-            nn.ConvTranspose2d(num_classes, ngf*4, 4, 2, 1),
+            nn.ConvTranspose2d(num_classes, ngf*4, 4, 1, 0),
             nn.BatchNorm2d(ngf*4),
             nn.LeakyReLU(0.2, inplace=True)
         )
         self.input_conv = nn.Sequential(
             # input is Z, going into a convolution
-            nn.ConvTranspose2d(nz, ngf*4, 4, 2, 1),
+            nn.ConvTranspose2d(nz, ngf*4, 4, 1, 0),
             nn.BatchNorm2d(ngf*4),
             nn.LeakyReLU(0.2, inplace=True)
         )
@@ -97,7 +97,11 @@ class CondCvGenerator(nn.Module):
             nn.BatchNorm2d(ngf * 2),
             nn.ReLU(True),
             # state size. (ngf*2) x 16 x 16
-            nn.ConvTranspose2d( ngf * 2, nc, 4, 2, 1, bias=False),
+            nn.ConvTranspose2d( ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            # state size. (ngf) x 32 x 32
+            nn.ConvTranspose2d( ngf, nc, 4, 2, 1, bias=False),
             nn.Tanh()
             # state size. (nc) x 64 x 64
         )
