9c290f301b6e99fa3a540eba9fd96f63b7839b4d
Pedro Gomes <phbgomes@hotmail.com>
2023-02-02 11:38:33 -0300

    remove noise=None so Identity call works

--- layers/ffc/ffc_bn_act.py
+++ layers/ffc/ffc_bn_act.py
@@ -87,8 +87,8 @@ class FFC_BN_ACT(nn.Module):
         debug_print(" -- Fim FFC_BN_ACT")
 
         ## Add Noise - PG
-        x_l = self.noise(x_l, noise=None)
+        x_l = self.noise(x_l)
         if type(x_g) != int:
-            x_g = self.noise(x_g, noise=None)
+            x_g = self.noise(x_g)
 
         return x_l, x_g
