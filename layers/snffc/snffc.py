f6c679558204a1f3fb43756288c0a8ab49b48aa6
Pedro Gomes <phbgomes@hotmail.com>
2023-02-14 10:59:07 -0300

    trying to print name modules g2g

--- layers/snffc/snffc.py
+++ layers/snffc/snffc.py
@@ -111,16 +111,12 @@ class SNFFC(FFC):
         self.convl2l = spectral_norm(self.convl2l)
         self.convg2l = spectral_norm(self.convg2l)
         
-        print(self.convl2g.children())
-        for name, layer in self.convl2g.named_modules():
+        print(self.convg2g.children())
+        for name, layer in self.convg2g.named_modules():
             print(name)
+
         self.convl2g = spectral_norm(self.convl2g) if isinstance(self.convl2g, nn.Conv2d) else self.convl2g
-       
-        for layer in self.convl2g.children():
-            print(layer)
-            if isinstance(layer, nn.Conv2d):
-                layer = spectral_norm(layer)
-       # self.convl2g = spectral_norm(self.convl2g)
+    
 
     def forward(self, x):
         x_l, x_g = x if type(x) is tuple else (x, 0)
