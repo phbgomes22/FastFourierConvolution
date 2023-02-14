24d20166d290c317861fee0a76266b9a7cc7a76f
Pedro Gomes <phbgomes@hotmail.com>
2023-02-14 11:13:18 -0300

    print model to check spectral norm

--- train_cond.py
+++ train_cond.py
@@ -56,6 +56,7 @@ def get_generator():
     if config.FFC_GENERATOR:
         netG = FFCCondGenerator(nz=nz, nc=nc, ngf=ngf, num_classes= num_classes, 
                                 embed_size=embed_size, uses_noise=True).to(device) 
+        print(netG)
     else:
         netG = CondCvGenerator(nz=nz, nc=nc, ngf=ngf, 
                         num_classes= num_classes, image_size=image_size, 
