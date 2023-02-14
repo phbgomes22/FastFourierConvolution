30d69370f00ffd23d8c99ecde858e03f31998361
Pedro Gomes <phbgomes@hotmail.com>
2023-02-14 10:43:18 -0300

    trying to add spectral norm in a different way

--- layers/snffc/snffc.py
+++ layers/snffc/snffc.py
@@ -98,16 +98,29 @@ class SN_FFC(nn.Module):
 
 ## Creating through inherintance
 class SNFFC(FFC):
+
+    def __init__(self, in_channels: int, out_channels: int, kernel_size: int,
+                 ratio_gin: float, ratio_gout: float, stride: int = 1, padding: int = 0,
+                 dilation: int = 1, groups: int = 1, bias: bool = False, enable_lfu: bool = True,
+                 attention: bool = False):
+
+        FFC.__init__(self, in_channels, out_channels, kernel_size, ratio_gin, ratio_gout, stride,
+                    padding, dilation, groups, bias, enable_lfu, attention)
+
+        self.convl2l = spectral_norm(self.convl2l)
+        self.convg2l = spectral_norm(self.convg2l)
+        self.convl2g = spectral_norm(self.convl2g)
+
     def forward(self, x):
         x_l, x_g = x if type(x) is tuple else (x, 0)
         out_xl, out_xg = 0, 0
 
         if self.ratio_gout != 1:
             # creates the output local signal passing the right signals to the right convolutions
-            out_xl = spectral_norm(self.convl2l(x_l)) + spectral_norm(self.convg2l(x_g))
+            out_xl = self.convl2l(x_l) + self.convg2l(x_g)
         if self.ratio_gout != 0:
             # creates the output global signal passing the right signals to the right convolutions
-            out_xg = spectral_norm(self.convl2g(x_l)) + self.convg2g(x_g)
+            out_xg = self.convl2g(x_l) + self.convg2g(x_g)
 
         # returns both signals as a tuple
         return out_xl, out_xg
