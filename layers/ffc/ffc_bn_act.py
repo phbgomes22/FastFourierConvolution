214496d6369a029221a983e2f06bc686a935ccc5
Pedro Gomes <phbgomes@hotmail.com>
2023-02-17 17:09:26 -0300

    add channel number to noise injection

--- layers/ffc/ffc_bn_act.py
+++ layers/ffc/ffc_bn_act.py
@@ -55,13 +55,14 @@ class FFC_BN_ACT(nn.Module):
                         ratio_gin, ratio_gout, stride, padding, dilation,
                         groups, bias, enable_lfu, attention=attention)
              
-
+        out_ch_l = int(out_channels * (1 - ratio_gout))
+        out_ch_g = int(out_channels * ratio_gout)
         # create the BatchNormalization layers
         lnorm = nn.Identity if ratio_gout == 1 else norm_layer
         gnorm = nn.Identity if ratio_gout == 0 else norm_layer
 
-        self.bn_l = lnorm(int(out_channels * (1 - ratio_gout)))
-        self.bn_g = gnorm(int(out_channels * ratio_gout))
+        self.bn_l = lnorm(out_ch_l)
+        self.bn_g = gnorm(out_ch_g)
 
         # create the activation function layers
         lact = nn.Identity if ratio_gout == 1 else activation_layer
@@ -73,9 +74,9 @@ class FFC_BN_ACT(nn.Module):
         self.print_size = Print(debug=Config.shared().DEBUG)
 
         ## Add Noise - PG
-        self.noise_l = NoiseInjection() if uses_noise else nn.Identity()
+        self.noise_l = NoiseInjection(out_ch_l) if uses_noise else nn.Identity()
         ## Add Noise - PG
-        self.noise_g = NoiseInjection() if uses_noise else nn.Identity()
+        self.noise_g = NoiseInjection(out_ch_g) if uses_noise else nn.Identity()
 
 
     def forward(self, x):
