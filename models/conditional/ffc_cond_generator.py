0b9cb98d0e071411f62c40ce063ba93dafa59cf7
Pedro Gomes <phbgomes@hotmail.com>
2023-02-28 09:33:44 -0300

    fix size last conv generator

--- models/conditional/ffc_cond_generator.py
+++ models/conditional/ffc_cond_generator.py
@@ -66,7 +66,7 @@ class FFCCondGenerator(FFCModel):
         # ) 
         # adds the last layer
         layers.append(
-            FFC_BN_ACT(ngf*1, nc, 3, 0.5, 0, stride=1, padding=1, 
+            FFC_BN_ACT(ngf*1, nc, 4, 0.5, 0, stride=2, padding=1,  #3, 0.5, 0, stride=1, padding=1
                        norm_layer=nn.Identity, activation_layer=nn.Tanh,
                        uses_noise=self.uses_noise, uses_sn=self.uses_sn, attention=True) 
         )
