fdf582d6ba0f4c26d68f308ad849ba6fbba61412
Pedro Gomes <phbgomes@hotmail.com>
2023-03-15 13:25:26 -0300

    pass code to colab

--- fgan_complete.py
+++ fgan_complete.py
@@ -16,6 +16,8 @@ import torch_fidelity
 
 
 def hinge_loss_dis(fake, real):
+    print(fake.dim())
+    print(fake.size())
     assert fake.dim() == 2 and fake.shape[1] == 1 and real.shape == fake.shape, f'{fake.shape} {real.shape}'
     loss = torch.nn.functional.relu(1.0 - real).mean() + \
            torch.nn.functional.relu(1.0 + fake).mean()
@@ -26,8 +28,6 @@ def hinge_loss_gen(fake):
     print(fake.dim())
     print(fake.size())
     fake = fake.squeeze(-1).squeeze(-1)
-    print(fake.dim())
-    print(fake.size())
     assert fake.dim() == 2 and fake.shape[1] == 1
     loss = -fake.mean()
     return loss
