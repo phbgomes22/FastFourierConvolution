5e78532d578b5e8f06632adc4fbce96c1b578848
Pedro Gomes <phbgomes@hotmail.com>
2023-03-21 16:58:02 -0300

    trying to fix spectral norm

--- layers/snffc/snffc_transpose.py
+++ layers/snffc/snffc_transpose.py
@@ -19,8 +19,14 @@ class SNFFCTranspose(FFCTranspose):
                     padding, dilation, groups, bias, enable_lfu, out_padding, attention)
 
         self.convl2l = spectral_norm(self.convl2l)
-        self.convg2l = spectral_norm(self.convg2l) if isinstance(self.convg2l, nn.Conv2d) else self.convg2l
-        self.convl2g = spectral_norm(self.convl2g) if isinstance(self.convl2g, nn.Conv2d) else self.convl2g
+        
+        if isinstance(self.convg2l, nn.Conv2d):
+            print("Added Spectral Nrom")
+            self.convg2l = spectral_norm(self.convg2l)
+        if isinstance(self.convl2g, nn.Conv2d):
+            print("Added Spectral Nrom")
+            self.convl2g = spectral_norm(self.convl2g)
+
         self.convg2gup = spectral_norm(self.convg2gup)
 
         # -- changing convg2g
