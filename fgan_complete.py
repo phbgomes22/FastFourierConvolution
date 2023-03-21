9e65da72603eaaa5ea207f672a4950c7f2403595
Pedro Gomes <phbgomes@hotmail.com>
2023-03-21 10:57:33 -0300

    fgan siminar to sngan

--- fgan_complete.py
+++ fgan_complete.py
@@ -88,18 +88,28 @@ class FDiscriminator(FFCModel):
     def __init__(self, sn=True):
         super(FDiscriminator, self).__init__()
         sn_fn = torch.nn.utils.spectral_norm if sn else lambda x: x
+        # 3, 4, 3, 4, 3, 4, 3
         self.main = torch.nn.Sequential(
             FFC_BN_ACT(in_channels=3, out_channels=64, kernel_size=3,
                 ratio_gin=0.0, ratio_gout=0.5, stride=1, padding=1, bias=False, 
                 uses_noise=False, uses_sn=True, activation_layer=nn.GELU),
-            FFC_BN_ACT(in_channels=64, out_channels=128, kernel_size=4,
+            FFC_BN_ACT(in_channels=64, out_channels=64, kernel_size=4,
                 ratio_gin=0.5, ratio_gout=0.5, stride=2, padding=1, bias=False, 
                 uses_noise=False, uses_sn=True, activation_layer=nn.GELU),
-            FFC_BN_ACT(in_channels=128, out_channels=256, kernel_size=4,
+            FFC_BN_ACT(in_channels=64, out_channels=128, kernel_size=3,
+                ratio_gin=0.5, ratio_gout=0.5, stride=1, padding=1, bias=False, 
+                uses_noise=False, uses_sn=True, activation_layer=nn.GELU),
+            FFC_BN_ACT(in_channels=128, out_channels=128, kernel_size=4,
                 ratio_gin=0.5, ratio_gout=0.5, stride=2, padding=1, bias=False, 
                 uses_noise=False, uses_sn=True, activation_layer=nn.GELU),
-            FFC_BN_ACT(in_channels=256, out_channels=512, kernel_size=4,
+            FFC_BN_ACT(in_channels=128, out_channels=256, kernel_size=3,
+                ratio_gin=0.5, ratio_gout=0.5, stride=1, padding=1, bias=False, 
+                uses_noise=False, uses_sn=True, activation_layer=nn.GELU),
+            FFC_BN_ACT(in_channels=256, out_channels=256, kernel_size=4,
                 ratio_gin=0.5, ratio_gout=0.5, stride=2, padding=1, bias=False, 
+                uses_noise=False, uses_sn=True, activation_layer=nn.GELU),
+            FFC_BN_ACT(in_channels=256, out_channels=512, kernel_size=3,
+                ratio_gin=0.5, ratio_gout=0.5, stride=1, padding=1, bias=False, 
                 uses_noise=False, uses_sn=True, activation_layer=nn.GELU),
             # FFC_BN_ACT(in_channels=512, out_channels=1, kernel_size=4,
             #     ratio_gin=0.5, ratio_gout=0, stride=1, padding=0, bias=False, 
