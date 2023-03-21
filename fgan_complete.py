7c2cac8bdf741fda2b677327ba18ce3e6118a2ec
Pedro Gomes <phbgomes@hotmail.com>
2023-03-21 13:10:29 -0300

    running metrics again

--- fgan_complete.py
+++ fgan_complete.py
@@ -322,28 +322,28 @@ def train(args):
 
         # check if it is validation time
         next_step = step + 1
-        if next_step % (args.num_epoch_steps/20) != 0:
+        if next_step % (args.num_epoch_steps) != 0:
             continue
         pbar.close()
         G.eval()
         print('Evaluating the generator...')
 
         # compute and log generative metrics
-        # metrics = torch_fidelity.calculate_metrics(
-        #     input1=torch_fidelity.GenerativeModelModuleWrapper(G, args.z_size, args.z_type, num_classes),
-        #     input1_model_num_samples=args.num_samples_for_metrics,
-        #     input2='cifar10-train',
-        #     isc=True,
-        #     fid=True,
-        #     kid=True,
-        #     ppl=False,
-        #     ppl_epsilon=1e-2,
-        #     ppl_sample_similarity_resize=64,
-        # )
+        metrics = torch_fidelity.calculate_metrics(
+            input1=torch_fidelity.GenerativeModelModuleWrapper(G, args.z_size, args.z_type, num_classes),
+            input1_model_num_samples=args.num_samples_for_metrics,
+            input2='cifar10-train',
+            isc=True,
+            fid=True,
+            kid=True,
+            ppl=False,
+            ppl_epsilon=1e-2,
+            ppl_sample_similarity_resize=64,
+        )
         
-        # # log metrics
-        # for k, v in metrics.items():
-        #     tb.add_scalar(f'metrics/{k}', v, global_step=next_step)
+        # log metrics
+        for k, v in metrics.items():
+            tb.add_scalar(f'metrics/{k}', v, global_step=next_step)
 
         # log observed images
         samples_vis = G(z_vis).detach().cpu()
@@ -353,10 +353,10 @@ def train(args):
         samples_vis.save(os.path.join(args.dir_logs, f'{next_step:06d}.png'))
 
         # save the generator if it improved
-        # if metric_greater_cmp(metrics[leading_metric], last_best_metric):
-        #     print(f'Leading metric {args.leading_metric} improved from {last_best_metric} to {metrics[leading_metric]}')
+        if metric_greater_cmp(metrics[leading_metric], last_best_metric):
+            print(f'Leading metric {args.leading_metric} improved from {last_best_metric} to {metrics[leading_metric]}')
 
-        #     last_best_metric = metrics[leading_metric]
+            last_best_metric = metrics[leading_metric]
 
         # resume training
         if next_step <= args.num_total_steps:
