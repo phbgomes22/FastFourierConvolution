e0fef319658e3fedd9b5027d5be36d50260924d9
Pedro Gomes <phbgomes@hotmail.com>
2023-03-23 17:09:25 -0300

    saving less metrics

--- fgan_complete.py
+++ fgan_complete.py
@@ -359,7 +359,7 @@ def train(args):
 
         # check if it is validation time
         next_step = step + 1
-        if next_step % (args.num_epoch_steps/4) != 0:
+        if next_step % (args.num_epoch_steps/2) != 0:
             continue
         pbar.close()
         G.eval()
