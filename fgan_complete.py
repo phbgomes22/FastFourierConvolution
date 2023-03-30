8212f8ae19406a7d7849f283e49e7d97dc684136
Pedro Gomes <phbgomes@hotmail.com>
2023-03-30 17:26:34 -0300

    less channels with linear

--- fgan_complete.py
+++ fgan_complete.py
@@ -83,7 +83,7 @@ class FGenerator(FFCModel):
 
         self.l1 = nn.Linear(z_size, self.mg * self.mg * self.ngf )
 
-        self.conv1 = FFC_BN_ACT(self.ngf*8, self.ngf*8, 4, 0.0, ratio_g, stride=2, padding=1, activation_layer=nn.GELU, 
+        self.conv1 = FFC_BN_ACT(self.ngf, self.ngf*8, 4, 0.0, ratio_g, stride=2, padding=1, activation_layer=nn.GELU, 
                       norm_layer=nn.BatchNorm2d, upsampling=True, uses_noise=True, uses_sn=True)
         self.lcl_noise1 = NoiseInjection(self.ngf*4)
         self.glb_noise1 = NoiseInjection(self.ngf*4)
