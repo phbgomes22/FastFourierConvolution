2295600a2fd6769199f69510f7512d74c954e797
Pedro Gomes <phbgomes@hotmail.com>
2023-04-25 13:33:54 -0300

    starts with g_in = 0

--- fgan_complete.py
+++ fgan_complete.py
@@ -84,7 +84,7 @@ class FGenerator(FFCModel):
         #               norm_layer=nn.BatchNorm2d, upsampling=True, uses_noise=True, uses_sn=True)
         # self.lcl_noise1 = NoiseInjection(self.ngf*4)
         # self.glb_noise1 = NoiseInjection(self.ngf*4)
-        self.conv2 = FFC_BN_ACT(self.ngf*8, self.ngf*4, 4, ratio_g, ratio_g, stride=2, padding=1, activation_layer=nn.GELU, 
+        self.conv2 = FFC_BN_ACT(self.ngf*8, self.ngf*4, 4, 0.0, ratio_g, stride=2, padding=1, activation_layer=nn.GELU, 
                       norm_layer=nn.BatchNorm2d, upsampling=True, uses_noise=True, uses_sn=True)
         self.lcl_noise2 = NoiseInjection(self.ngf*2)
         self.glb_noise2 = NoiseInjection(self.ngf*2)
@@ -104,9 +104,9 @@ class FGenerator(FFCModel):
     def forward(self, z):
         
         fake = self.noise_to_feature(z)
-        print(fake.size())
+      
         fake = fake.reshape(fake.size(0), -1, self.mg, self.mg)
-        print(fake.size())
+       
 
 
         # fake = self.conv1(z.view(-1, self.z_size, 1, 1))
