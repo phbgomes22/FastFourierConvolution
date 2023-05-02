fd522cf37c44874564e3fcbeac77388a5f73650e
Pedro Gomes <phbgomes@hotmail.com>
2023-05-02 16:22:48 -0300

    reverting to 1 dim embed

--- fgan_cond_complete.py
+++ fgan_cond_complete.py
@@ -118,7 +118,7 @@ class Discriminator(torch.nn.Module):
         self.mg = mg
         sn_fn = torch.nn.utils.spectral_norm if sn else lambda x: x
 
-    # self.conv1 = sn_fn(torch.nn.Conv2d(3 + 1, 64, 3, stride=1, padding=(1,1)))
+        self.conv1 = sn_fn(torch.nn.Conv2d(3 + 1, 64, 3, stride=1, padding=(1,1)))
         self.conv2 = sn_fn(torch.nn.Conv2d(64, 64, 4, stride=2, padding=(1,1)))
         self.conv3 = sn_fn(torch.nn.Conv2d(64, 128, 3, stride=1, padding=(1,1)))
         self.conv4 = sn_fn(torch.nn.Conv2d(128, 128, 4, stride=2, padding=(1,1)))
@@ -132,36 +132,30 @@ class Discriminator(torch.nn.Module):
         ## == Conditional
         self.label_embed = nn.Embedding(num_classes, 32*32)
 
-        self.label_conv = nn.Sequential(
-            nn.ConvTranspose2d(1, 32, 4, 2, 1),
-     #       nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.1)
-        )
+    #     self.label_conv = nn.Sequential(
+    #         nn.ConvTranspose2d(1, 32, 4, 2, 1),
+    #  #       nn.BatchNorm2d(32),
+    #         nn.LeakyReLU(0.1)
+    #     )
 
-        self.input_conv = nn.Sequential(
-            nn.ConvTranspose2d(3, 32, 4, 2, 1, bias=False),
-        #    nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.1)
-        )
+    #     self.input_conv = nn.Sequential(
+    #         nn.ConvTranspose2d(3, 32, 4, 2, 1, bias=False),
+    #     #    nn.BatchNorm2d(32),
+    #         nn.LeakyReLU(0.1)
+    #     )
 
     def forward(self, x, labels):
         labels = torch.unsqueeze(labels, dim=-1)
         labels = torch.unsqueeze(labels, dim=-1)
         embedding = self.label_embed(labels)
         embedding = embedding.view(labels.shape[0], 1, 32, 32)
-        #
-        embedding = self.label_conv(embedding)
+        # embedding = self.label_conv(embedding)
 
-        #
-        input = self.input_conv(x)
-
-      #  input = torch.cat([x, embedding], dim=1)
-        input = torch.cat([input, embedding], dim=1)
-
-      #  m = self.act(self.conv1(input))
-      #  m = self.act(self.conv2(m))
-
-        m = self.act(self.conv2(input))
+        # input = self.input_conv(x)
+        input = torch.cat([x, embedding], dim=1)
+        
+        m = self.act(self.conv1(input))
+        m = self.act(self.conv2(m))
         m = self.act(self.conv3(m))
         m = self.act(self.conv4(m))
         m = self.act(self.conv5(m))
