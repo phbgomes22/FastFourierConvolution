b62842a33a97d1596a2de191211d9528b3c0c202
Pedro Gomes <phbgomes@hotmail.com>
2023-05-02 10:22:09 -0300

    trying to fix identity error in convg2g

--- layers/ffc/ffc.py
+++ layers/ffc/ffc.py
@@ -98,7 +98,9 @@ class FFC(nn.Module):
             out_xl = self.convl2l(x_l) + self.convg2l(x_g)
         if self.ratio_gout != 0:
             # creates the output global signal passing the right signals to the right convolutions
-            out_xg = self.convl2g(x_l) + self.convg2g(x_g, y)
+            out_xg = self.convl2g(x_l) 
+            if type(self.convg2g) is not nn.Identity:
+              out_xg = out_xg + self.convg2g(x_g, y)
 
         # returns both signals as a tuple
         return out_xl, out_xg
