abbf35f594a2a5942c2d46d92145230fda7fe71f
Pedro Gomes <phbgomes@hotmail.com>
2023-05-03 09:39:23 -0300

    debugging fcondgenerator

--- fgan_cond_complete.py
+++ fgan_cond_complete.py
@@ -94,8 +94,9 @@ class FCondGenerator(FFCModel):
         print(z.size())
       #  input = torch.cat([z, embedding], dim=1)
        # input = input.view(labels.shape[0], -1, 1, 1)
+        
+        input = self.noise_to_feature(z)
         print(input.size())
-        input = self.noise_to_feature(input)
       #  input = self.input_conv(z)
        # input = fake.reshape(input.size(0), -1, self.mg, self.mg)
 
