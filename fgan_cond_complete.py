01783665cfe5227f2cf8074c9a687822fe1cf8b8
Pedro Gomes <phbgomes@hotmail.com>
2023-05-06 16:52:35 -0300

    fix noise injection call - more generic

--- fgan_cond_complete.py
+++ fgan_cond_complete.py
@@ -139,15 +139,15 @@ class FCondGenerator(FFCModel):
 
         self.conv2 = FFC_BN_ACT(self.ngf*8, self.ngf*4, 4, 0.0, ratio_g, stride=2, padding=1, activation_layer=nn.GELU, 
                       norm_layer=ConditionalBatchNorm2d, upsampling=True, uses_noise=True, uses_sn=True, num_classes=num_classes)
-        self.lcl_noise2 = NoiseInjection(self.ngf*2)
+        self.lcl_noise2 = NoiseInjection(self.ngf*4*ratio_g)
         
         self.conv3 = FFC_BN_ACT(self.ngf*4, self.ngf*2, 4, ratio_g, ratio_g, stride=2, padding=1, activation_layer=nn.GELU, 
                       norm_layer=ConditionalBatchNorm2d, upsampling=True, uses_noise=True, uses_sn=True, num_classes=num_classes)
-        self.lcl_noise3 = NoiseInjection(self.ngf)
+        self.lcl_noise3 = NoiseInjection(self.ngf*2*ratio_g)
         
         self.conv4 = FFC_BN_ACT(self.ngf*2, self.ngf, 4, ratio_g, ratio_g, stride=2, padding=1, activation_layer=nn.GELU, 
                       norm_layer=ConditionalBatchNorm2d, upsampling=True, uses_noise=True, uses_sn=True, num_classes=num_classes)
-        self.lcl_noise4 = NoiseInjection(self.ngf//2)
+        self.lcl_noise4 = NoiseInjection(self.ngf*ratio_g)
         
         self.conv5 = FFC_BN_ACT(self.ngf, 3, 3, ratio_g, 0.0, stride=1, padding=1, activation_layer=nn.Tanh, 
                        norm_layer=nn.Identity, upsampling=False, uses_noise=True, uses_sn=True)
@@ -187,6 +187,7 @@ class FCondGenerator(FFCModel):
         ## remainder
         fake = self.conv2(input, labels)
         if self.training:
+
             fake = self.lcl_noise2(fake[0]), fake[1] 
         
         fake = self.conv3(fake, labels)
