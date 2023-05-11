770bffe5e5b5d3578b3b75c3fef54b9fcf9a7af7
Pedro Gomes <phbgomes@hotmail.com>
2023-05-10 21:35:05 -0300

    basic weight init

--- fgan_cond_complete.py
+++ fgan_cond_complete.py
@@ -25,12 +25,7 @@ def weights_init(m):
     '''
     classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        if hasattr(m, "weight"):
-            torch.nn.init.xavier_uniform_(m.weight)
-        if hasattr(m, "bias"):
-            if hasattr(m.bias, "data"):       
-                m.bias.data.fill_(0)
-        # nn.init.normal_(m.weight.data, 0.0, 0.02)
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
         nn.init.constant_(m.bias.data, 0)
