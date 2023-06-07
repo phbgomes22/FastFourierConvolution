b3cab1221d01940004f3d0758985b0c5e605b36b
Pedro Gomes <phbgomes@hotmail.com>
2023-06-07 12:00:01 -0300

    test getting index

--- mimicry_master/torch_mimicry/training/trainer.py
+++ mimicry_master/torch_mimicry/training/trainer.py
@@ -345,9 +345,9 @@ class Trainer:
                #                                global_step=global_step)
                 self.scheduler_d.step()
                 self.scheduler_g.step()
-                _lr_D = self.scheduler_d.get_last_lr()
+                _lr_D = self.scheduler_d.get_last_lr()[0]
                 
-                _lr_G = self.scheduler_g.get_last_lr()
+                _lr_G = self.scheduler_g.get_last_lr()[0]
                 print(_lr_D, _lr_G)
                 log_data.add_metric('lr_D', _lr_D, group='lr', precision=6)
                 log_data.add_metric('lr_G', _lr_G, group='lr', precision=6)
