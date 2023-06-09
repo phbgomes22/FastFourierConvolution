fa2ab65d09dd5c5bfd5c116dd5bb1608653da0bf
Pedro Gomes <pedrogomes@MacBook-Pro-de-Pedro.local>
2023-06-08 23:49:06 -0300

    running stl on alpha 0.5 for chart on paper

--- fgan_complete.py
+++ fgan_complete.py
@@ -73,7 +73,7 @@ class FGenerator(FFCModel):
         super(FGenerator, self).__init__()
         self.z_size = z_size
         self.ngf = 64
-        ratio_g = 0.25
+        ratio_g = 0.5#0.25
         self.mg = mg
 
         sn_fn = torch.nn.utils.spectral_norm 
