19fcca54067b8f95767d54674e822ce4d1e96500
Pedro Gomes <phbgomes@hotmail.com>
2023-06-14 17:10:03 -0300

    add cpu

--- test.py
+++ test.py
@@ -159,7 +159,7 @@ def get_filters(args):
     
     img, outputs = model(z)
 
-    save_image(img, args.dir_logs, 1, 'example_image')
+    save_image(img.cpu(), args.dir_logs, 1, 'example_image')
 
     #print feature_maps
     for feature_map in outputs:
@@ -188,7 +188,7 @@ def save_image(fake, logs, num, name='image'):
     generated_image = np.transpose(fake, (1,2,0))
     # generated_image -= generated_image.min()
     # generated_image /= generated_image.max()
-    im = Image.fromarray(generated_image.numpy().astype(np.uint8)) #.squeeze(axis=2).numpy() * 255).astype(np.uint8)
+    im = Image.fromarray(generated_image.cpu().numpy().astype(np.uint8)) #.squeeze(axis=2).numpy() * 255).astype(np.uint8)
     im.save(os.path.join(logs, name + str(num) + ".png"))
 
 def test(args):
